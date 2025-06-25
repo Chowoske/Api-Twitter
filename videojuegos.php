f953a5e9b13418bc223738c74eea12677ecc6266
Im Going To Kill You <Chowoske@gmail.com>
2025-06-24 21:40:47 -0600

    Add files via upload

---
+++ videojuegos.php
@@ -0,0 +1,260 @@
+<?php
+$video_juegos = [
+    [
+        "id" => 1,
+        "Name" => "Celeste",
+        "Price" => "20 USD",
+        "Release Date" => "2018-01-25",
+        "Gender" => "Plataformas, Aventura",
+        "Available In" => "Steam, Epic Games Store, Nintendo eShop, PlayStation Store, Xbox Store"
+    ],
+    [
+        "id" => 2,
+        "Name" => "Doki Doki Literature Club",
+        "Price" => "Gratis",
+        "Release Date" => "2017-09-22",
+        "Gender" => "Visual Novel, Horror psicológico",
+        "Available In" => "Steam"
+    ],
+    [
+        "id" => 3,
+        "Name" => "Night in the Woods",
+        "Price" => "20 USD",
+        "Release Date" => "2017-02-21",
+        "Gender" => "Aventura, Narrativa",
+        "Available In" => "Steam, GOG, PlayStation Store, Xbox Store, Nintendo eShop"
+    ],
+    [
+        "id" => 4,
+        "Name" => "Purrfect Apawcalypse",
+        "Price" => "Gratis",
+        "Release Date" => "2020-10-31",
+        "Gender" => "Simulación, Estrategia",
+        "Available In" => "Steam"
+    ],
+    [
+        "id" => 5,
+        "Name" => "Oneshot",
+        "Price" => "15 USD",
+        "Release Date" => "2016-12-08",
+        "Gender" => "Aventura, Puzzle",
+        "Available In" => "Steam, GOG"
+    ],
+    [
+        "id" => 6,
+        "Name" => "Shovel Knight",
+        "Price" => "20 USD",
+        "Release Date" => "2014-06-26",
+        "Gender" => "Plataformas, Acción",
+        "Available In" => "Steam, Nintendo eShop, PlayStation Store, Xbox Store, GOG"
+    ],
+    [
+        "id" => 7,
+        "Name" => "Cry of Fear",
+        "Price" => "Gratis",
+        "Release Date" => "2013-04-12",
+        "Gender" => "Survival Horror",
+        "Available In" => "Steam"
+    ],
+    [
+        "id" => 8,
+        "Name" => "Hotline Miami 2",
+        "Price" => "15 USD",
+        "Release Date" => "2015-03-10",
+        "Gender" => "Acción, Shooter",
+        "Available In" => "Steam, GOG, PlayStation Store"
+    ],
+    [
+        "id" => 9,
+        "Name" => "The Binding of Isaac: Rebirth",
+        "Price" => "20 USD",
+        "Release Date" => "2014-11-04",
+        "Gender" => "Roguelike, Acción",
+        "Available In" => "Steam, Xbox Store, Nintendo eShop, PlayStation Store"
+    ],
+    [
+        "id" => 10,
+        "Name" => "Milk inside a bag of milk",
+        "Price" => "8 USD",
+        "Release Date" => "2019-04-01",
+        "Gender" => "Aventura, Horror psicológico",
+        "Available In" => "Steam"
+    ],
+    [
+        "id" => 11,
+        "Name" => "Cuphead",
+        "Price" => "20 USD",
+        "Release Date" => "2017-09-29",
+        "Gender" => "Run and Gun, Plataformas",
+        "Available In" => "Steam, Xbox Store, Epic Games Store"
+    ],
+    [
+        "id" => 12,
+        "Name" => "Undertale",
+        "Price" => "15 USD",
+        "Release Date" => "2015-09-15",
+        "Gender" => "RPG, Aventura",
+        "Available In" => "Steam, GOG, PlayStation Store, Nintendo eShop"
+    ],
+    [
+        "id" => 13,
+        "Name" => "Nine Sols",
+        "Price" => "20 USD",
+        "Release Date" => "2023-03-01",
+        "Gender" => "Metroidvania, Acción",
+        "Available In" => "Steam, GOG"
+    ],
+    [
+        "id" => 14,
+        "Name" => "Fear & Hunger",
+        "Price" => "10 USD",
+        "Release Date" => "2018-06-05",
+        "Gender" => "Survival Horror, RPG",
+        "Available In" => "Steam"
+    ],
+    [
+        "id" => 15,
+        "Name" => "Hyper Light Drifter",
+        "Price" => "20 USD",
+        "Release Date" => "2016-03-31",
+        "Gender" => "Aventura, Acción",
+        "Available In" => "Steam, GOG, Xbox Store, PlayStation Store, Nintendo eShop"
+    ],
+    [
+        "id" => 16,
+        "Name" => "Fran Bow",
+        "Price" => "15 USD",
+        "Release Date" => "2015-09-25",
+        "Gender" => "Aventura, Horror psicológico",
+        "Available In" => "Steam, GOG"
+    ],
+    [
+        "id" => 17,
+        "Name" => "Terraria",
+        "Price" => "10 USD",
+        "Release Date" => "2011-05-16",
+        "Gender" => "Aventura, Sandbox",
+        "Available In" => "Steam, Xbox Store, PlayStation Store, Nintendo eShop, Mobile Stores"
+    ],
+    [
+        "id" => 18,
+        "Name" => "Mouthwash",
+        "Price" => "15 USD",
+        "Release Date" => "2021-07-15",
+        "Gender" => "Aventura, Puzzle",
+        "Available In" => "Steam"
+    ],
+    [
+        "id" => 19,
+        "Name" => "Hotline Miami",
+        "Price" => "10 USD",
+        "Release Date" => "2012-10-23",
+        "Gender" => "Acción, Shooter",
+        "Available In" => "Steam, GOG, PlayStation Store"
+    ],
+    [
+        "id" => 20,
+        "Name" => "A Colorful Tale",
+        "Price" => "15 USD",
+        "Release Date" => "2022-02-18",
+        "Gender" => "Plataformas, Aventura",
+        "Available In" => "Steam, Nintendo eShop"
+    ],
+    [
+        "id" => 21,
+        "Name" => "Dead Cells",
+        "Price" => "25 USD",
+        "Release Date" => "2018-08-07",
+        "Gender" => "Metroidvania, Acción",
+        "Available In" => "Steam, GOG, Xbox Store, PlayStation Store, Nintendo eShop"
+    ],
+    [
+        "id" => 22,
+        "Name" => "Enter the Gungeon",
+        "Price" => "15 USD",
+        "Release Date" => "2016-04-05",
+        "Gender" => "Roguelike, Shooter",
+        "Available In" => "Steam, GOG, Xbox Store, PlayStation Store, Nintendo eShop"
+    ],
+    [
+        "id" => 23,
+        "Name" => "Hollow Knight",
+        "Price" => "15 USD",
+        "Release Date" => "2017-02-24",
+        "Gender" => "Metroidvania, Aventura",
+        "Available In" => "Steam, GOG, Nintendo eShop, Xbox Store, PlayStation Store"
+    ],
+    [
+        "id" => 24,
+        "Name" => "Cave Story",
+        "Price" => "10 USD",
+        "Release Date" => "2004-12-20",
+        "Gender" => "Aventura, Plataformas",
+        "Available In" => "Steam, Nintendo eShop"
+    ],
+    [
+        "id" => 25,
+        "Name" => "Oneshot",
+        "Price" => "15 USD",
+        "Release Date" => "2016-12-08",
+        "Gender" => "Aventura, Puzzle",
+        "Available In" => "Steam, GOG"
+    ]
+];
+
+$itemsPorPagina = 5;
+$totalJuegos = count($video_juegos);
+$paginaActual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+if ($paginaActual < 1) $paginaActual = 1;
+$inicio = ($paginaActual - 1) * $itemsPorPagina;
+$juegosPagina = array_slice($video_juegos, $inicio, $itemsPorPagina);
+$totalPaginas = ceil($totalJuegos / $itemsPorPagina);
+
+$formato = (isset($_GET['formato']) && $_GET['formato'] === 'json') ? 'json' : 'html';
+
+if ($formato === 'json') {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode([
+        "pagina" => $paginaActual,
+        "totalPaginas" => $totalPaginas,
+        "juegos" => $juegosPagina
+    ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <title>Videojuegos Indies - Página <?= $paginaActual ?></title>
+    <link rel="stylesheet" href="estilo.css" />
+</head>
+
+<body>
+    <h1>Videojuegos Indies - Página <?= $paginaActual ?></h1>
+    <ul>
+        <?php foreach ($juegosPagina as $juego): ?>
+            <li>
+                <strong><?= htmlspecialchars($juego['Name']) ?></strong><br>
+                Precio: <?= htmlspecialchars($juego['Price']) ?><br>
+                Lanzamiento: <?= htmlspecialchars($juego['Release Date']) ?><br>
+                Género: <?= htmlspecialchars($juego['Gender']) ?><br>
+                Disponible en: <?= htmlspecialchars($juego['Available In']) ?><br>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+    <nav>
+        <?php for ($i = 1; $i <= $totalPaginas; $i++): ?>
+            <?php if ($i == $paginaActual): ?>
+                <strong><?= $i ?></strong>
+            <?php else: ?>
+                <a href="?pagina=<?= $i ?>"><?= $i ?></a>
+            <?php endif; ?>
+            &nbsp;
+        <?php endfor; ?>
+    </nav>
+</body>
+</html>
